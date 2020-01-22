4bcb29b13c4d64e3fb0bc0b4008ad19f7e44364f
Mac <storiaca@gmail.com>
2020-01-22 16:38:33 +0100

    Refactoring Code into a Service

--- 05-factories-and-services/Code/UsingFactoriesAndServices/Begin/app/controllers/ordersController.js
+++ 05-factories-and-services/Code/UsingFactoriesAndServices/Begin/app/controllers/ordersController.js
@@ -1,17 +1,17 @@
 (function() {
-  var OrdersController = function($scope, $routeParams, customersFactory) {
+  var OrdersController = function($scope, $routeParams, customersService) {
     var customerId = $routeParams.customerId;
     $scope.orders = null;
 
     function init() {
       //Search the customers for the customerId
-      $scope.customer = customersFactory.getCustomer(customerId);
+      $scope.customer = customersService.getCustomer(customerId);
     }
 
     init();
   };
 
-  OrdersController.$inject = ["$scope", "$routeParams", "customersFactory"];
+  OrdersController.$inject = ["$scope", "$routeParams", "customersService"];
 
   angular
     .module("customersApp")
