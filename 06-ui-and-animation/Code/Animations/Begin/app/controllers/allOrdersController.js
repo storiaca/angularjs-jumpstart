7d9a9d0d2b6b370f91fbe32296da6645de214b40
Mac <storiaca@gmail.com>
2020-01-24 16:42:12 +0100

    Adding bootstrap classes

--- 06-ui-and-animation/Code/Animations/Begin/app/controllers/allOrdersController.js
+++ 06-ui-and-animation/Code/Animations/Begin/app/controllers/allOrdersController.js
@@ -1,35 +1,36 @@
 (function() {
-    
-    var AllOrdersController = function ($scope, customersFactory) {
-        $scope.orders = null;
-        $scope.ordersTotal = 0.0;
-        $scope.totalType;
-        
-        function init() {
-             customersFactory.getOrders()
-                .then(function(response) {
-                    $scope.orders = response.data;
-                    getOrdersTotal();
-                }, function(data, status, headers, config) {
-                    //handle error
-                });
-        }        
-        
-        function getOrdersTotal() {
-            var total = 0;
-            for (var i=0,len=$scope.orders.length;i<len;i++) {
-                total += $scope.orders[i].total;
-            }
-            $scope.ordersTotal = total;
-            $scope.totalType = ($scope.ordersTotal > 100) ? 'success' : 'danger';
+  var AllOrdersController = function($scope, customersFactory) {
+    $scope.orders = null;
+    $scope.ordersTotal = 0.0;
+    $scope.totalType;
+
+    function init() {
+      customersFactory.getOrders().then(
+        function(response) {
+          $scope.orders = response.data;
+          getOrdersTotal();
+        },
+        function(data, status, headers, config) {
+          //handle error
         }
+      );
+    }
 
-        init();
-    };
-    
-    AllOrdersController.$inject = ['$scope', 'customersFactory'];
+    function getOrdersTotal() {
+      var total = 0;
+      for (var i = 0, len = $scope.orders.length; i < len; i++) {
+        total += $scope.orders[i].total;
+      }
+      $scope.ordersTotal = total;
+      $scope.totalType = $scope.ordersTotal > 100 ? "success" : "danger";
+    }
 
-    angular.module('customersApp')
-      .controller('AllOrdersController', AllOrdersController);
-    
-}());
+    init();
+  };
+
+  AllOrdersController.$inject = ["$scope", "customersFactory"];
+
+  angular
+    .module("customersApp")
+    .controller("AllOrdersController", AllOrdersController);
+})();
